6d2d353e7ad94f60c8481d9248ebab59dd0e4d45
Kevin Kim <kkim@Kevs-X10-MBP-2.local>
2018-04-30 22:22:03 -0400

    +Portfolio - Style Tweaks

--- docs/js/main.js
+++ docs/js/main.js
@@ -9,24 +9,32 @@ $(".sliding-link").click(function(e) {
 
 $(document).ready(function() {
     
+    // On page load, any items that are 'on screen', make visible.
+    fadeCheck();
+    
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
     
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-            
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},1500);
-                    
-            }
-            
-        }); 
+        // As items 'enter screen', make visible.
+        fadeCheck();
     
     });
+
+    function fadeCheck() {
+    /* Check the location of each desired element */
+    $('.hideme').each( function(i){
+
+        var bottom_of_object = $(this).position().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+        /* If the object is completely visible in the window, fade it in */
+        if( bottom_of_window > bottom_of_object ){
+            
+            $(this).animate({'opacity':'1'},1500);
+                
+        }
+        
+    }); 
+    }
     
 });
